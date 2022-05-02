5386b301c4f2eb646b0ba828c679b66fff6fe34f
tishanjegels <95338764+tishanjegels@users.noreply.github.com>
2022-05-02 19:36:47 +0200

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+function myValidation(){
+    
+    let userInput = document.getElementById("newtask").value;
+    
+    if (true) {
+        document.getElementById("error").style.display = "block";
+     }
+
+     const emptylist = document.createElement('li')
+
+     const textNode = document.createTextNode(input)
+
+     emptyList.appendChild(textNode) 
+
+     document.getAnimations("myTaskList").appendChild(emptyList)
+     
+      
+
+     
+}
